4d888b7d82570070adb8718187da4b83f24d52e5
Castro380 <mateuso@iesb.edu.br>
2023-09-20 19:35:42 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  PaperProvider
+} from 'react-native-paper';
+import Router from './src/routes/Router';
+
+
+export default function App() {
+
+  return (
+    <PaperProvider>
+      <Router />
+    </PaperProvider>
+  );
+}
+
+
+
+
